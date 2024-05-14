df6d7924f97a0aeabd080898e0d3b94e336fb0d8
bstriker <strikeforcezero@gmail.com>
2024-05-13 22:41:35 -0400

    feat: working implementation with bevy main

--- examples/example/src/main.rs
+++ examples/example/src/main.rs
@@ -1,16 +1,11 @@
-use bevy::log::{BoxedSubscriber, LogPlugin};
+use bevy::log::LogPlugin;
 use bevy::prelude::*;
 
 use bevy_frame_count_subscriber::plugin::FrameCountSubscriberPlugin;
-use bevy_frame_count_subscriber::subscriber::update_subscriber;
 
 fn main() {
     App::new()
-        // .add_plugins(DefaultPlugins.build().disable::<LogPlugin>())
-        .add_plugins(DefaultPlugins.set(LogPlugin {
-            update_subscriber: Some(update_subscriber),
-            ..default()
-        }))
+        .add_plugins(DefaultPlugins.build().disable::<LogPlugin>())
         .add_plugins(FrameCountSubscriberPlugin)
         .add_systems(Update, || info!("test"))
         .run()
--- src/subscriber.rs
+++ src/subscriber.rs
@@ -1,39 +1,12 @@
-
-
-use std::io::{stderr};
-
-
-
-
-use bevy::log::{BoxedSubscriber, LogPlugin};
+use bevy::log::{BoxedLayer, LogPlugin};
 use bevy::prelude::*;
-use bevy::utils::tracing::{Subscriber};
-
-use tracing_log::LogTracer;
-
-
-use tracing_subscriber::layer::{SubscriberExt};
-use tracing_subscriber::registry::LookupSpan;
-use tracing_subscriber::{EnvFilter, Layer, Registry};
-
+use tracing_subscriber::Layer;
 
 use crate::config::FrameCountSubscriberConfig;
 use crate::formatter::FrameCounterPrefixFormatter;
 
-pub fn update_subscriber(subscriber: BoxedSubscriber) -> BoxedSubscriber {
-    Box::new(subscriber.with(create_layer(None)))
-}
-
-fn create_layer<S: Subscriber + for<'a> LookupSpan<'a>>(
-    config: Option<&FrameCountSubscriberConfig>,
-) -> impl Layer<S> {
-    tracing_subscriber::fmt::Layer::default()
-        .event_format(create_filter(config))
-        .with_writer(stderr)
-}
-
-fn create_filter_from_app(app: &App) -> FrameCounterPrefixFormatter {
-    create_filter(app.world.get_resource::<FrameCountSubscriberConfig>())
+pub(crate) fn create_filter_from_app(app: &App) -> FrameCounterPrefixFormatter {
+    create_filter(app.world().get_resource::<FrameCountSubscriberConfig>())
 }
 
 pub(crate) fn create_filter(
@@ -47,35 +20,20 @@ pub(crate) fn create_filter(
     frame_counter_prefix_formatter
 }
 
-pub(crate) fn register_subscriber(config: Option<&FrameCountSubscriberConfig>) {
-    /// derived from https://github.com/bevyengine/bevy/blob/dedf66f72bd8659b744e12b341a7f8de4ed8ba17/crates/bevy_log/src/lib.rs#L129-L228 (MIT/APACHE)
-    let finished_subscriber;
-    let default_log_plugin = LogPlugin::default();
-    let default_filter = { format!("{},{}", default_log_plugin.level, default_log_plugin.filter) };
-    let filter_layer = EnvFilter::try_from_default_env()
-        .or_else(|_| EnvFilter::try_new(&default_filter))
-        .unwrap();
-
-    // create new subscriber with log level filter
-    let subscriber = Registry::default().with(filter_layer);
-
-    // create format layer and replace event_formatter with frame count injector
+fn custom_layer(app: &mut App) -> Option<BoxedLayer> {    // create format layer and replace event_formatter with frame count injector
     let fmt_layer = tracing_subscriber::fmt::Layer::default()
-        .event_format(create_filter(config))
+        .event_format(create_filter_from_app(app))
         .with_writer(std::io::stderr);
 
-    let subscriber = subscriber.with(fmt_layer);
-    finished_subscriber = subscriber;
-    let logger_already_set = LogTracer::init().is_err();
-    let subscriber_already_set =
-        tracing::subscriber::set_global_default(finished_subscriber).is_err();
+    Some(Box::new(vec![
+        /* rustfmt multi line */
+        fmt_layer.boxed(),
+    ]))
+}
 
-    match (logger_already_set, subscriber_already_set) {
-        (true, true) => warn!(
-                "Could not set global logger and tracing subscriber as they are already set. Consider disabling LogPlugin."
-            ),
-        (true, _) => warn!("Could not set global logger as it is already set. Consider disabling LogPlugin."),
-        (_, true) => warn!("Could not set global tracing subscriber as it is already set. Consider disabling LogPlugin."),
-        _ => (),
-    }
+pub(crate) fn add_log_plugin_with_custom_layer(app: &mut App) -> &mut App {
+    app.add_plugins(LogPlugin {
+        custom_layer,
+        ..default()
+    })
 }
